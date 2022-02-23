89de3acd18f124051902a50f76a06e206f585cbd
Julius Garrido <jflgarrido@gmail.com>
2022-02-23 12:48:42 -0700

    in progress

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,29 @@
 import React from "react";
 import {Route, Switch} from "react-router-dom";
+import NavBar from "./NavBar"
+import Home from "./Home"
+import CharacterList from "./CharacterList"
+import NewCharacter from "./NewCharacter"
 
 function App() {
     return (
         <div>
-            Hello World
+            <NavBar />
+            <Switch>
+                <Route path="/characters">
+                    <CharacterList />
+                </Route>
+                <Route path="/characters/new">
+                    <NewCharacter />
+                </Route>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route path="*">
+                    <h1>404 not found</h1>
+                    <h1>Babe, you're lost!</h1>
+                </Route>
+            </Switch>
         </div>
     )
 }
