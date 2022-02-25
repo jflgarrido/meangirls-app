b429bdd2e778ea46c33beb474c8d8057c27e4797
Julius Garrido <jflgarrido@gmail.com>
2022-02-25 11:47:07 -0700

    in progress MVP + stretch

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,11 +1,18 @@
 import React from "react";
 
-function CharacterCard({id, name, image, quote}) {
+function CharacterCard({id, name, image, quote, onRemoveCard}) {
+    function handleDeleteClick(){
+        fetch(`http://localhost:3001/characters/${id}`, {
+            method: "DELETE",
+        });
+        onRemoveCard(id)
+    }
     return(
         <div>
             <h3>{name}</h3>
             <img src={image} alt={name} />
             <h3>{quote}</h3>
+            <button onClick={handleDeleteClick}>DELETE</button>
         </div>
     )
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -3,12 +3,16 @@ import CharacterCard from "./CharacterCard"
 
 function CharacterList({characters, setCharacters}) {
     
-
     useEffect(() => {
         fetch("http://localhost:3001/characters")
         .then(r => r.json())
         .then(data => setCharacters(data))
     }, [])
+
+    function handleRemoveCard(id){
+        const newList = characters.filter((card) => card.id !== id);
+        setCharacters(newList);
+    }
 
     const characterCards = characters.map((character) => {
         console.log(character)
@@ -18,6 +22,7 @@ function CharacterList({characters, setCharacters}) {
             name={character.name}
             image={character.image}
             quote={character.quote}
+            onRemoveCard={handleRemoveCard}
         />
         
     });
