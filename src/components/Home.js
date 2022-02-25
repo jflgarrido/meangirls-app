c386edc4f17e290cfca9eeffb68418efd729cfc6
Julius Garrido <jflgarrido@gmail.com>
2022-02-25 13:43:01 -0700

    MVP + readme

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,8 @@ import React from "react"
 function Home() {
     return (
         <section>
-            <h1>
-                Welcome to Mean Girls App!
-            </h1>
+            <h1 className="comic">Welcome to Mean Girls App!</h1>
+            <img src="https://media.giphy.com/media/3o7aTy3ePwrk5D3bHO/giphy.gif" alt="Mean Girls"/>
         </section>
     )
 }
